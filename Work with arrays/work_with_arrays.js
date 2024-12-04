eb0153ec03f99078bea33fa6252d874dbf75c26b
lapoosick <alka03@mail.ru>
2024-12-04 16:25:16 +0700

    Work with arrays: переименован namespace.

--- Work with arrays/work_with_arrays.js
+++ Work with arrays/work_with_arrays.js
@@ -1,13 +1,13 @@
 "use strict";
 
-window.MYSPACE = window.MYSPACE || {};
-window.MYSPACE.numbersArray = [0, -4, 18, 3, -1, 2];
+window.ARRAYS_SPACE = window.ARRAYS_SPACE || {};
+window.ARRAYS_SPACE.numbersArray = [0, -4, 18, 3, -1, 2];
 
-window.MYSPACE.getEvenNumbers = function (array) {
+window.ARRAYS_SPACE.getEvenNumbers = function (array) {
     return array.filter(e => e % 2 === 0);
 };
 
-window.MYSPACE.createOneHundredNumbersArray = function () {
+window.ARRAYS_SPACE.createOneHundredNumbersArray = function () {
     const array = [];
 
     for (let i = 1; i <= 100; i++) {
@@ -18,27 +18,27 @@ window.MYSPACE.createOneHundredNumbersArray = function () {
 };
 
 (function sortArrayDesc() {
-    MYSPACE.numbersArray.sort(function (e1, e2) {
+    ARRAYS_SPACE.numbersArray.sort(function (e1, e2) {
         return e2 - e1;
     });
 
-    console.log("Массив, отсортированный по убыванию: [" + MYSPACE.numbersArray.join(", ") + "]");
+    console.log("Массив, отсортированный по убыванию: [" + ARRAYS_SPACE.numbersArray.join(", ") + "]");
 })();
 
 (function getFirstFiveElements() {
-    console.log("Первые пять элементов массива: [" + MYSPACE.numbersArray.slice(0, 5).join(", ") + "]");
+    console.log("Первые пять элементов массива: [" + ARRAYS_SPACE.numbersArray.slice(0, 5).join(", ") + "]");
 })();
 
 (function getLastFiveElements() {
-    console.log("Последние пять элементов массива: [" + MYSPACE.numbersArray.slice(-5).join(", ") + "]");
+    console.log("Последние пять элементов массива: [" + ARRAYS_SPACE.numbersArray.slice(-5).join(", ") + "]");
 })();
 
 (function getEvenNumbersSum() {
     console.log("Сумма элементов массива, которые являются чётными числами: " +
-        MYSPACE.getEvenNumbers(MYSPACE.numbersArray).reduce((sum, current) => sum + current, 0));
+        ARRAYS_SPACE.getEvenNumbers(ARRAYS_SPACE.numbersArray).reduce((sum, current) => sum + current, 0));
 })();
 
 (function getEvenNumbersSquares() {
     console.log("Список квадратов чётных чисел от 1 до 100: "
-        + MYSPACE.getEvenNumbers(MYSPACE.createOneHundredNumbersArray()).map(e => e * e).join(", "));
+        + ARRAYS_SPACE.getEvenNumbers(ARRAYS_SPACE.createOneHundredNumbersArray()).map(e => e * e).join(", "));
 })();
