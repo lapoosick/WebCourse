319d22b9dcf8c8b4761d39cb6b9d80aedbe24700
lapoosick <alka03@mail.ru>
2024-12-17 10:09:21 +0700

    Work with arrays: исправлено согласно замечаниям преподавателя.

--- WorkWithArrays/work_with_arrays.js
+++ WorkWithArrays/work_with_arrays.js
@@ -3,18 +3,16 @@
 (function () {
     const numbersArray = [0, -4, 18, 3, -1, 2];
 
-    function sortArrayDesc(array) {
+    function sortArrayDescending(array) {
         array.sort((e1, e2) => e2 - e1);
-
-        console.log("Массив, отсортированный по убыванию: [" + array.join(", ") + "]");
     }
 
-    function getFiveFirstElements(array) {
-        console.log("Первые пять элементов массива: [" + array.slice(0, 5).join(", ") + "]");
+    function getFirstElements(array, elementsCount) {
+        return array.slice(0, elementsCount);
     }
 
-    function getFiveLastElements(array) {
-        console.log("Последние пять элементов массива: [" + array.slice(-5).join(", ") + "]");
+    function getLastElements(array, elementsCount) {
+        return array.slice(-elementsCount);
     }
 
     function getEvenNumbers(array) {
@@ -22,8 +20,7 @@
     }
 
     function getEvenNumbersSum(array) {
-        console.log("Сумма элементов массива, которые являются чётными числами: " +
-            getEvenNumbers(array).reduce((sum, number) => sum + number, 0));
+        return getEvenNumbers(array).reduce((sum, number) => sum + number, 0);
     }
 
     function createOneHundredNumbersArray() {
@@ -37,12 +34,14 @@
     }
 
     function getEvenNumbersSquares(array) {
-        console.log("Список квадратов чётных чисел от 1 до 100: " + getEvenNumbers(array).map(e => e * e).join(", "));
+        return getEvenNumbers(array).map(e => e * e);
     }
 
-    sortArrayDesc(numbersArray);
-    getFiveFirstElements(numbersArray);
-    getFiveLastElements(numbersArray);
-    getEvenNumbersSum(numbersArray);
-    getEvenNumbersSquares(createOneHundredNumbersArray());
+    sortArrayDescending(numbersArray);
+
+    console.log("Массив, отсортированный по убыванию: [" + numbersArray.join(", ") + "]");
+    console.log("Первые пять элементов массива: [" + getFirstElements(numbersArray, 5).join(", ") + "]");
+    console.log("Последние пять элементов массива: [" + getLastElements(numbersArray, 5).join(", ") + "]");
+    console.log("Сумма элементов массива, которые являются чётными числами: " + getEvenNumbersSum(numbersArray));
+    console.log("Список квадратов чётных чисел от 1 до 100: " + getEvenNumbersSquares(createOneHundredNumbersArray()).join(", "));
 })();
