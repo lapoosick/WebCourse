9ab785d72a2159a7ee72d02f06c2bc1dbfa17c4f
lapoosick <alka03@mail.ru>
2024-12-20 09:20:21 +0700

    Classes: initial commit.

---
+++ Classes/script.js
@@ -0,0 +1,68 @@
+"use strict";
+
+(function () {
+    function Animal(name) {
+        this.name = name;
+    }
+
+    Animal.prototype.speak = function () {
+        console.log(`${this.name} издаёт звук.`);
+    }
+
+    function Dog(name) {
+        Animal.call(this, name);
+    }
+
+    Dog.prototype.speak = function () {
+        console.log(`${this.name} лает.`);
+    }
+
+    function Cat(name) {
+        Animal.call(this, name);
+    }
+
+    Cat.prototype.speak = function () {
+        console.log(`${this.name} мяукает.`);
+    }
+
+    Object.setPrototypeOf(Dog.prototype, Animal.prototype);
+    Object.setPrototypeOf(Cat.prototype, Animal.prototype);
+
+    const animal = new Animal("Животное");
+    const dog = new Dog("Шарик");
+    const cat = new Cat("Мурка");
+
+    animal.speak();
+    dog.speak();
+    cat.speak();
+
+    class AnimalClass {
+        constructor(name) {
+            this.name = name;
+        }
+
+        speak() {
+            console.log(`${this.name} издаёт звук в классе.`);
+        }
+    }
+
+    class DogClass extends AnimalClass {
+        speak() {
+            console.log(`${this.name} лает в классе.`);
+        }
+    }
+
+    class CatClass extends AnimalClass {
+        speak() {
+            console.log(`${this.name} мяукает в классе.`);
+        }
+    }
+
+    const classAnimal = new AnimalClass("Зверь");
+    const classDog = new DogClass("Бобик");
+    const classCat = new CatClass("Мурзик");
+
+    classAnimal.speak();
+    classDog.speak();
+    classCat.speak();
+})();
